b0c66ec1e9f6b0d3d44a005b8a5ed880bb890e69
T.Kuramata <t_kuramata@yahoo.co.jp>
2016-06-16 18:23:14 +0900

    コード整形

--- PersonalDeviceApp/index.js
+++ PersonalDeviceApp/index.js
@@ -8,23 +8,21 @@ var serialport = require('serialport');
 var http_src = fs.readFileSync('./index.html');		// HTMLファイルのソースを同期処理で読み出す
 
 // HTTPサーバーを作成
-var app = http.createServer(function(req, res) {
+var app = http.createServer(function (req, res) {
 	
 	// リクエストされたURLを取得
 	var url_parts = url.parse(req.url);
 	console.log(url_parts.pathname);
 	
 	// ルートまたはindex.htmlの場合
-	if(url_parts.pathname == '/' || url_parts.pathname == '/index.html')
-	{
-		res.writeHead(200, {'Content-Type': 'text/html'});
+	if (url_parts.pathname == '/' || url_parts.pathname == '/index.html') {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
 		res.write(http_src);
 		res.end();
 	}
 	
 	// その他のファイルは404コードを返答する
-	else
-	{
+	else {
 		res.writeHead(404);
 		res.write(url_parts.pathname + "not found.");	// 脆弱性
 		res.end();
@@ -37,7 +35,7 @@ var sp;
 
 // ■■■■■■■■　socket.io（サーバー側）　■■■■■■■■■
 var io = require('socket.io').listen(app);
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function (socket) {
 	
 	// なぜか.htmlにアクセス時に'connection'イベントが発生する（原因不明）
 	console.log("socket.io connected.");
@@ -51,7 +49,7 @@ io.sockets.on('connection', function(socket) {
 	*/
 	
 	// 接続開始時のイベントハンドラを定義
-	socket.on('open-connection', function(data) {
+	socket.on('open-connection', function (data) {
 		
 		console.log("socket.io received 'open-connection' event and '" + data + "' message from html");
 		
@@ -65,34 +63,34 @@ io.sockets.on('connection', function(socket) {
 		});
 		
 		// 受信イベントハンドラを定義
-		sp.on('data', function(recv) {
+		sp.on('data', function (recv) {
 			console.log('recv:' + recv);
 			
 			// 受信データをファイルに書き出し
-			fs.appendFile('zw.csv', recv, 'utf8', function(err) {
-				if(! err) {
+			fs.appendFile('zw.csv', recv, 'utf8', function (err) {
+				if (!err) {
 					fs.appendFileSync('zw.csv', '\n', 'utf8');
 				} else {
-					console.log('recv_err:' + err);					
+					console.log('recv_err:' + err);
 				}
 			});
 			
 			// 受信データをHTMLへ送信
-			socket.emit('response', recv);	
+			socket.emit('response', recv);
 			
 		});
 		
-	});	
+	});
 	
 	// クライアントからシリアルデータの送信要求イベントに対するハンドラ
-	socket.on('path-through', function(data) {
+	socket.on('path-through', function (data) {
 		
 		console.log();
 		console.log("socket.io received 'path-through' event and '" + data + "' message from html");
 		
 		// シリアルデータを送信
-		sp.write(data, function(err, results) {
-			if (! err) {	// エラーなし
+		sp.write(data, function (err, results) {
+			if (!err) {	// エラーなし
 				console.log(results + ' bytes written');
 			} else {		// エラーあり
 				console.log("error : " + err + "  " + results);
@@ -100,10 +98,10 @@ io.sockets.on('connection', function(socket) {
 		});
 	});
 	
-			
+	
 	// 使用可能なCOMポートを書き出す
 	serialport.list(function (err, ports) {
-		ports.forEach(function(port) {
+		ports.forEach(function (port) {
 			console.log(port.comName);
 		});
 	})
@@ -115,7 +113,7 @@ io.sockets.on('connection', function(socket) {
 	// python実行のためのchild_processを作成
 	var spawn = require('child_process').spawn;
 	
-	socket.on('old_path-through', function(data) {
+	socket.on('old_path-through', function (data) {
 		
 		console.log();
 		console.log("socket.io received 'path-through' event and '" + data + "' message from html");
@@ -125,7 +123,7 @@ io.sockets.on('connection', function(socket) {
 		console.log("var py = spawn('python', ['zw.py', data]);");
 		
 		// pythonからの受信イベントを登録
-		py.stdout.on('data', function(data){
+		py.stdout.on('data', function (data) {
 			console.log("py.stdout.on('data', ... " + data);
 			//socket.broadcast.emit('response', data);
 			socket.emit('response', data);
@@ -136,12 +134,12 @@ io.sockets.on('connection', function(socket) {
 			console.log('stderr: ' + data);
 		});
 		
-		py.on('exit', function(){
+		py.on('exit', function () {
 			console.log("exit event");
 		});
 	});
 	
-	socket.on('abc', function(data) {
+	socket.on('abc', function (data) {
 		
 		console.log();
 		console.log("socket.io received 'abc' event and '" + data + "' message from html");
@@ -151,7 +149,7 @@ io.sockets.on('connection', function(socket) {
 		console.log("var py = spawn('python', ['test.py']);");
 		
 		// pythonからの受信イベントを登録
-		py.stdout.on('data', function(data){
+		py.stdout.on('data', function (data) {
 			console.log("py.stdout.on('data', ... " + data);
 		});
 		
@@ -160,7 +158,7 @@ io.sockets.on('connection', function(socket) {
 			console.log('stderr: ' + data);
 		});
 		
-		py.on('exit', function(){
+		py.on('exit', function () {
 			console.log("exit event");
 		});
 	});
@@ -171,9 +169,9 @@ io.sockets.on('connection', function(socket) {
 // ■■■■■■■■　socket.io-client（クライアント側）　■■■■■■■■■
 var client = require('socket.io-client');
 var cli_socket = client.connect('http://localhost:3000');
-cli_socket.on('connect',function(socket){
+cli_socket.on('connect', function (socket) {
 	cli_socket.send('how are you?');
-	cli_socket.on('msg', function(data) {
+	cli_socket.on('msg', function (data) {
 		//cli_socket.emit('abc', "thank you");
 		console.log('to client' + data);
 	});
