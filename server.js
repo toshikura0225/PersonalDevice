78b54132b6fb0246798cbd47a9105d4f81a4ebc3
toshikura0225 <t_kuramata@yahoo.co.jp>
2016-08-16 10:50:59 +0900

    ver

--- server.js
+++ server.js
@@ -2,51 +2,38 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var http_src = fs.readFileSync('./index.html');
-var js_src = fs.readFileSync('./js/my_script.js');
-var js_2 = fs.readFileSync('./mys.js');
+// ■■■■■■■■　Node.js　■■■■■■■■■
+var http_src = fs.readFileSync('./index.html');		// HTMLファイルのソースを同期処理で読み出す
+var js_src = fs.readFileSync('./script.js');
 
-var rasp_src= fs.readFileSync('./rasp.html');
-var ando_src= fs.readFileSync('./ando.html');
-
-var app = http.createServer(function(req, res) {
+// HTTPサーバーを作成
+var app = http.createServer(function (req, res) {
 	
+	// リクエストされたURLを取得
 	var url_parts = url.parse(req.url);
-	
 	console.log(url_parts.pathname);
 	
-	if(url_parts.pathname == '/' || url_parts.pathname == '/index.html')
-	{
-		res.writeHead(200, {'Content-Type': 'text/html'});
+	// ルートまたはindex.htmlの場合
+	if (url_parts.pathname == '/' || url_parts.pathname == '/index.html') {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
 		res.write(http_src);
 		res.end();
 	}
-	else if(url_parts.pathname == '/js/my_script.js')
-	{
-		res.writeHead(200, {'Content-Type': 'text/javascript'});
+
+	else if (url_parts.pathname == '/script.js') {
+		res.writeHead(200, { 'Content-Type': 'text/javascript' });
 		res.write(js_src);
 		res.end();
 	}
-	else if(url_parts.pathname == '/mys.js')
-	{
-		res.writeHead(200, {'Content-Type': 'text/javascript'});
-		res.write(js_2);
-		res.end();
-	}
-	else if(url_parts.pathname == '/rasp.html')
-	{
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(rasp_src);
-		res.end();
-	}
-	else if(url_parts.pathname == '/ando.html')
-	{
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(ando_src);
-		res.end();
-	}
 	
-}).listen(process.env.PORT || 3000);
+	// その他のファイルは404コードを返答する
+	else {
+		res.writeHead(404);
+		res.write(url_parts.pathname + "not found.");	// 脆弱性
+		res.end();
+	}
+
+}).listen(process.env.PORT || 3000);	// サーバー内環境のポートまたは3000番で待受
 
 var io = require('socket.io').listen(app);
 io.sockets.on('connection', function(socket) {
