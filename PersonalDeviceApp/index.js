988f6e41fb15895419d776b3009e208d870673d7
LaVie PC <t_kuramata@yahoo.co.jp>
2016-08-20 17:34:14 +0900

    ifconfigコマンド成功

--- PersonalDeviceApp/index.js
+++ PersonalDeviceApp/index.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var serialport = require('serialport');
 
 
 // ■■■■■■■■　Node.js　■■■■■■■■■
@@ -28,6 +27,13 @@ var app = http.createServer(function (req, res) {
 		res.end();
 	}
 	
+	else if (url_parts.pathname == '/cam1.jpg') {
+		var img_src = fs.readFileSync('./cam1.jpg');
+		res.writeHead(200, { 'Content-Type': 'image' });
+		res.write(img_src);
+		res.end();
+	}
+	
 	// その他のファイルは404コードを返答する
 	else {
 		res.writeHead(404);
@@ -37,8 +43,6 @@ var app = http.createServer(function (req, res) {
 
 }).listen(process.env.PORT || 3000);	// サーバー内環境のポートまたは3000番で待受
 
-// シリアルポートのインスタンス
-var sp;
 
 // ■■■■■■■■　socket.io（サーバー側）　■■■■■■■■■
 var io = require('socket.io').listen(app);
@@ -55,63 +59,7 @@ io.sockets.on('connection', function (socket) {
 	});
 	*/
 	
-	// 接続開始時のイベントハンドラを定義
-	socket.on('open-connection', function (data) {
-		
-		   
-		// シリアルポートのインスタンスを作成する
-		sp = new serialport(data, {
-			baudRate: 9600,
-			dataBits: 8,
-			parity: 'none',
-			stopBits: 1,
-			flowControl: true,
-		});
-		
-		// 受信イベントハンドラを定義
-		sp.on('data', function (recv) {
-			console.log('recv:' + recv);
-			
-			// 受信データをファイルに書き出し
-			fs.appendFile('zw.csv', recv, 'utf8', function (err) {
-				if (!err) {
-					fs.appendFileSync('zw.csv', '\n', 'utf8');
-				} else {
-					console.log('recv_err:' + err);
-				}
-			});
-			
-			// 受信データをHTMLへ送信
-			socket.emit('response', recv);
-			
-		});
-		
-	});
-	
-	// クライアントからシリアルデータの送信要求イベントに対するハンドラ
-	socket.on('path-through', (data) => {
-		
-		console.log();
-		console.log("socket.io received 'path-through' event and '" + data + "' message from html");
-		
-		// シリアルデータを送信
-		sp.write(data, function (err, results) {
-			if (!err) {	// エラーなし
-				console.log(results + ' bytes written');
-			} else {		// エラーあり
-				console.log("error : " + err + "  " + results);
-			}
-		});
-	});
-	
-	
-	// 使用可能なCOMポートを書き出す
-	serialport.list(function (err, ports) {
-		ports.forEach(function (port) {
-			console.log(port.comName);
-		});
-	})
-	
+	/*
 	// コマンドライン操作（セキュリティ最低...）
 	const exec = require('child_process').exec;
 	
@@ -129,64 +77,7 @@ io.sockets.on('connection', function (socket) {
 		});
 
 	});
-	
-	
-	// ～～～～～　OLDコード　～～～～～
-	
-	// python実行のためのchild_processを作成
-	var spawn = require('child_process').spawn;
-	
-	socket.on('old_path-through', function (data) {
-		
-		console.log();
-		console.log("socket.io received 'path-through' event and '" + data + "' message from html");
-		
-		// pythonを実行
-		var py = spawn('python', ['zw.py', data]);
-		console.log("var py = spawn('python', ['zw.py', data]);");
-		
-		// pythonからの受信イベントを登録
-		py.stdout.on('data', function (data) {
-			console.log("py.stdout.on('data', ... " + data);
-			//socket.broadcast.emit('response', data);
-			socket.emit('response', data);
-		});
-		
-		// pythonからエラーイベントを受信時
-		py.stderr.on('data', function (data) {
-			console.log('stderr: ' + data);
-		});
-		
-		py.on('exit', function () {
-			console.log("exit event");
-		});
-	});
-	
-	socket.on('abc', function (data) {
-		
-		console.log();
-		console.log("socket.io received 'abc' event and '" + data + "' message from html");
-		
-		// pythonを実行
-		var py = spawn('python', ['test.py', 'my_arg']);
-		console.log("var py = spawn('python', ['test.py']);");
-		
-		// pythonからの受信イベントを登録
-		py.stdout.on('data', function (data) {
-			console.log("py.stdout.on('data', ... " + data);
-		});
-		
-		// pythonからエラーイベントを受信時
-		py.stderr.on('data', function (data) {
-			console.log('stderr: ' + data);
-		});
-		
-		py.on('exit', function () {
-			console.log("exit event");
-		});
-	});
-	
-	
+	*/
 });
 
 // ■■■■■■■■　socket.io-client（クライアント側）　■■■■■■■■■
@@ -198,15 +89,16 @@ cli_socket.on('connect', function (socket) {
 	cli_socket.on('path-through', function (data) {
 		console.log('to client' + data);
 		//exec(data,
-		exec("131106_ConnectionSupporter.exe",
+		//exec("131106_ConnectionSupporter.exe",
+		exec("ifconfig",
 		//{cwd: 'C:\\Users\\Toshihiro\\Desktop\\PersonalDevice\\PersonalDeviceApp'},
-		(error, stdout, stderr) => {
+		function(error, stdout, stderr) {
 			if (error) {
-				console.error(`exec error: ${error}`);
+				console.error('exec error:' + error);
 				return;
 			}
-			console.log(`stdout: ${stdout}`);
-			console.log(`stderr: ${stderr}`);
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
 		});
 
 	});
